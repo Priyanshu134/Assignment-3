1da58993e48551135a216eeb20441f64a3d7ebef
Priyanshu134 <120158521+Priyanshu134@users.noreply.github.com>
2023-01-13 00:12:36 +0530

    Update A3Q7.py

--- A3Q7.py
+++ A3Q7.py
@@ -1,16 +1,12 @@
-fib = [1,1]
-print("input upto which number sequence is required :" , end=" ")
-n = int(input())
-Sum = 2
-length = 2
-for i in range(3,n+1) :
-    fib.append(fib[i-3]+fib[i-2])
-    Sum += fib[i-1]
-    length += 1
-if n == 1 :
-    print([1])
-    print(1.0)
-else :
-
-    print(fib)
-    print(Sum/length)
+n=int(input("enter no. upto which you want to ge the series"))
+n1=0
+n2=1
+sum=0
+if n<=0:
+    print("enter no. greater than 0")
+else:
+    for i in range(0,n):
+        print(sum,end=" ")
+        n1=n2
+        n2=sum
+        sum=n1+n2
